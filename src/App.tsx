0fc476c2d3795ad448a53870cc246c9fcc9da743
rodrigoaugusto99 <rodrigoaugusto51@outlook.com>
2024-03-30 20:03:31 -0300

    add: ThemeDark, ThemeToggle, menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
 import './global.css'
 
-import { RouterProvider } from 'react-router-dom'
-
-import { router } from './routes'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
+import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'sonner'
+
+import { ThemeProvider } from './components/theme/theme-provider'
+import { router } from './routes'
 
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | pizza.shop" />
-      <Toaster richColors />
-      <RouterProvider router={router} />
+      <ThemeProvider defaultTheme="dark" storageKey="pizzashop-theme">
+        <Helmet titleTemplate="%s | pizza.shop" />
+
+        <Toaster richColors />
+
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </HelmetProvider>
   )
 }
