ee1fd565d5a3e5baf9b363832e458f8c21f1393c
rodrigoaugusto99 <rodrigoaugusto51@outlook.com>
2024-03-30 00:24:10 -0300

    add: layouts

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,16 +1,29 @@
 import { createBrowserRouter } from 'react-router-dom'
-
+import { AppLayout } from './pages/_layouts/app'
+import { AuthLayout } from './pages/_layouts/auth'
 import { Dashboard } from './pages/app/dashboard'
 import { SignIn } from './pages/auth/sign-in'
 
 //a funcao recebe um array com um objeto pra cada rota 
 export const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Dashboard />,
-  },
-  {
-    path: '/sign-in',
-    element: <SignIn />,
-  },
-])
+    {
+      path: '/',
+      element: <AppLayout />,
+      children: [
+        {
+          path: '/',
+          element: <Dashboard />,
+        },
+      ],
+    },
+    {
+      path: '/',
+      element: <AuthLayout />,
+      children: [
+        {
+          path: '/sign-in',
+          element: <SignIn />,
+        },
+      ],
+    },
+  ])
