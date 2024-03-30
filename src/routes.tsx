1a2c91b8a20ff874691d53c3240a92202de6e771
rodrigoaugusto99 <rodrigoaugusto51@outlook.com>
2024-03-30 02:39:02 -0300

    add: cadastro de restaurante

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,6 +3,7 @@ import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
 import { Dashboard } from './pages/app/dashboard'
 import { SignIn } from './pages/auth/sign-in'
+import { SignUp } from './pages/auth/sign-up'
 
 //a funcao recebe um array com um objeto pra cada rota 
 export const router = createBrowserRouter([
@@ -23,6 +24,10 @@ export const router = createBrowserRouter([
         {
           path: '/sign-in',
           element: <SignIn />,
+        },
+        {
+          path: '/sign-up',
+          element: <SignUp />,
         },
       ],
     },
