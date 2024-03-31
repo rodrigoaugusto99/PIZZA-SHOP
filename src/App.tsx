274b0a2e7110b300c88a4a4a8cd03e7296e9de3c
rodrigoaugusto99 <rodrigoaugusto51@outlook.com>
2024-03-31 19:22:35 -0300

    add: configurando API Client

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import { Toaster } from 'sonner'
 
 import { ThemeProvider } from './components/theme/theme-provider'
 import { router } from './routes'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './lib/react-query'
 
 export function App() {
   return (
@@ -14,8 +16,10 @@ export function App() {
         <Helmet titleTemplate="%s | pizza.shop" />
 
         <Toaster richColors />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
 
-        <RouterProvider router={router} />
       </ThemeProvider>
     </HelmetProvider>
   )
